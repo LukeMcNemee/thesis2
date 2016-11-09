7a827c26031da3eaa04c00dcaa9b978416855ccb
LukeMcNemee <nemecl@seznam.cz>
2016-11-09 15:10:28 +0100

    quant code with functions

--- code/quant.r
+++ code/quant.r
@@ -2,9 +2,8 @@ require(matrixStats)
 setwd("~/Dokumenty/studijni_materialy/Edu-hoc/src")
 
 dat1 = read.delim("RssiApp/values.txt", header = FALSE, sep = ";", comment.char = "&")
-
 dat2 = read.delim("RssiApp2/values.txt", header = FALSE, sep = ";", comment.char = "&")
-datS = read.delim("RssiAppSniffer/values.txt", header = FALSE, sep = ";", comment.char = "&")
+dat3 = read.delim("RssiAppSniffer/values.txt", header = FALSE, sep = ";", comment.char = "&")
 
 basicStats <- function(x){
   m = mean(x)
@@ -32,98 +31,84 @@ quantization <- function(stats, values){
   return(list("x"=na.omit(x),"y"=na.omit(y)))
 }
 
-alpha <- 0.5
-#for (i in 1:423){ 
-i<-1
-  values1 <- as.numeric(dat1[i,1:1000]) -256
-  #m1 = mean(values1)
-  #sd1 = sd(values1)
+mapLocations <- function(locations, bits){
+  result = c(NA)
   
-  #qp1 = m1 + alpha * sd1
-  #qm1 = m1 - alpha * sd1
+  for(i in locations){
+    result = c(result, bits$y[which(bits$x == i)])
+  }
+  return(na.omit(result))
+}
+
+inc <- function(x){
+  eval.parent(substitute(x <- x + 1))
+}
+
+distance <- function( a, b){
+  d <- 0;
+  if(length(a) == 0 || length(b) == 0){
+    return(max(length(a), length(b)))
+  }
+  a <- na.omit(a)
+  b <- na.omit(b)
+  if(length(a) <= length(b)){
+    for(i in 1:length(a)){
+      if(a[i] != b[i]){
+        inc(d)
+      }
+    }
+  } else {
+    for(i in 1:length(b)){
+      if(a[i] != b[i]){
+        inc(d)
+      }
+    }
+  }
+  return(d)
+}
+
+lengths <- c(NA)
+h  <- c(NA)
+hS <- c(NA)
+
+alpha <- 0.5
+for (i in 1:423){ 
+#i<-2
+  values1 <- as.numeric(dat1[i,1:1000]) -256
+  values2 <- as.numeric(dat2[i,1:1000]) -256
+  values3 <- as.numeric(dat3[i,1:1000]) -256
   
   s1 <- basicStats(values1)
+  r1 <- quantization(s1, values1)
   
-  m1 <- s1[1]
+  s2 <- basicStats(values2)
+  r2 <- quantization(s2, values2)
   
-  y1 = c(NA)
-  x1 = c(NA)
-  for (j in 1:1000){
-    if(values1[j] > qp1){
-      y1 <- c(y1,1)
-      x1 <- c(x1,j)
-    } else if (values1[j] < qm1){
-      y1 <- c(y1,0)
-      x1 <- c(x1,j)
-    } else {
-      #bits <- c(bits,0)
-    }
+  s3 <- basicStats(values3)
+  r3 <- quantization(s3, values3)
+  
+  #plot(r1$x, r1$y, col = "red", pch=0,  ylim=c(-0.1,1.1))
+  #points(r2$x, r2$y+0.02, col = "blue", pch=1)
+  #points(r3$x, r3$y+0.04, col = "green", pch=2)
+  
+  
+  x_locations = intersect(r1$x,r2$x)
+  s_locations = intersect(x_locations,r1$x)
+  
+  bits1 <- mapLocations(x_locations, r1)
+  bits2 <- mapLocations(x_locations, r2)
+  bits3 <- mapLocations(s_locations, r3)
+  
+  lengths <- c(lengths, length(bits1))
+  if(length(bits1) != 0){
+    h <- c(h, distance(bits1, bits2))
+    hS <- c(hS, distance(bits1, bits3))
   }
+  #points(x_locations, bits1 -0.02, col = "red", pch=15)
+  #points(x_locations, bits2 -0.04, col = "blue", pch=16)
+  #points(s_locations, bits3 -0.06, col = "green", pch=17)
   
-  values2 <- as.numeric(dat2[i,1:1000]) -256
-  m2 = mean(values2)
-  sd2 = sd(values2)
-  
-  qp2 = m2 + alpha * sd2
-  qm2 = m2 - alpha * sd2
-  
-  y2 = c(NA)
-  x2 = c(NA)
-  for (j in 1:1000){
-    if(values2[j] > qp2){
-      y2 <- c(y2,1)
-      x2 <- c(x2,j)
-    } else if (values2[j] < qm2){
-      y2 <- c(y2,0)
-      x2 <- c(x2,j)
-    } else {
-      #bits <- c(bits,0)
-    }
-  }
-  
-  
-  valuesS <- as.numeric(datS[i,1:1000]) -256
-  mS = mean(valuesS)
-  sdS = sd(valuesS)
-  
-  qpS = mS + alpha * sdS
-  qmS = mS - alpha * sdS
-  
-  yS = c(NA)
-  xS = c(NA)
-  for (j in 1:1000){
-    if(valuesS[j] > qpS){
-      yS <- c(yS,1)
-      xS <- c(xS,j)
-    } else if (values2[j] < qmS){
-      yS <- c(yS,0)
-      xS <- c(xS,j)
-    } else {
-      #bits <- c(bits,0)
-    }
-  }
-  
-  plot(na.omit(x1), na.omit(y1), col = "red", pch=0,  ylim=c(-0.1,1.1))
-  points(na.omit(x2), na.omit(y2)+0.02, col = "blue", pch=1)
-  points(na.omit(xS), na.omit(yS)+0.04, col = "green", pch=2)
-  
-  
-  xres = intersect(x1,x2)
-  xresS = intersect(x1,xS)
-  yres1 = c(NA)
-  yres2 = c(NA)
-  yresS = c(NA)
-  for(j in xres){
-    yres1  = c(yres1, y1[which(x1 == j)])
-    yres2  = c(yres2, y2[which(x2 == j)])
-    
-  }
-  
-  for(j in xresS){
-    yresS  = c(yresS, yS[which(xS == j)])
-  }
-  points(na.omit(xres), na.omit(yres1) -0.02, col = "red", pch=15)
-  points(na.omit(xres), na.omit(yres2) -0.04, col = "blue", pch=16)
-  points(na.omit(xresS), na.omit(yresS) -0.06, col = "green", pch=17)
-  
-#}
+  #paste(distance(bits1, bits2),"/", length(bits1))
+  #paste(distance(bits1, bits3),"/", length(bits1))
+
+}
